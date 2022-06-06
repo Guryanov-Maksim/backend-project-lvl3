334bf15c96c80ad4f6056ab40c09c34f1913d861
Maksim-Guryanov <guryanov-mv@yandex.ru>
2022-06-06 10:32:12 +0300

    refactor: change cli args and options in according to requirements

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -4,10 +4,10 @@ import { program } from 'commander';
 import loadPageAndGetSavedPagePath from '../src/index.js'; // eslint-disable-line
 
 program
-  .description('Download a web page html')
+  .description('Page loader utility')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .argument('<direcorypath>', 'directory for saving a loaded page html')
   .argument('<url>', 'a page address that will be used to download the page html')
+  .option('-o, --output [dir]', 'output dir', process.cwd())
   .action(async (direcorypath, url) => {
     const savedPagePath = await loadPageAndGetSavedPagePath(direcorypath, url);
     console.log(savedPagePath);
