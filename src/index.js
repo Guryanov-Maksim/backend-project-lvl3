75aa6a7a3f40b78cb738d57e87575988818ef7df
Maksim-Guryanov <guryanov-mv@yandex.ru>
2022-06-16 14:28:30 +0300

    feat: add an ability to download images

--- src/index.js
+++ src/index.js
@@ -1,44 +1,92 @@
 import axios from 'axios';
-import { writeFile } from 'fs/promises';
+import { writeFile, mkdir } from 'fs/promises';
 import path from 'path';
 import * as yup from 'yup';
+import * as cheerio from 'cheerio';
 
 const schema = yup.string().url();
 
 const validateUrl = (url) => schema.validateSync(url);
 
-const downloadPage = async (pageAddress) => {
-  const url = new URL(pageAddress);
-  const { origin, pathname } = url;
+const getImageLinks = (dom) => {
+  const links = [];
+  dom('img').each((i, element) => {
+    links.push(dom(element).attr('src'));
+  });
+  return links;
+};
+
+const createPathToAssetsFileName = (link, nameBasePart, assetsDirectory) => {
+  const filteredName = link
+    .split('/')
+    .filter((pathPart) => !!pathPart)
+    .join('-');
+  const fileName = `${nameBasePart}-${filteredName}`;
+  const pathToSavedImageFile = path.join(assetsDirectory, fileName);
+  return pathToSavedImageFile;
+};
+
+const getReplacedDom = (dom, basePart, assetsDirectoryName) => {
+  dom('img').each((i, element) => {
+    const oldSrc = dom(element).attr('src');
+    const assetsFileName = createPathToAssetsFileName(oldSrc, basePart, assetsDirectoryName);
+    dom(element).attr('src', assetsFileName);
+  });
+  return dom;
+};
+
+const downloadPageImages = async ({ origin }, links) => {
+  const promises = links.map((link) => axios.get(link, { baseURL: origin, responseType: 'stream' }));
+  const responses = await Promise.all(promises);
+  return responses;
+};
+
+const saveContent = (fullPath, content) => writeFile(fullPath, content);
+
+const downloadPage = async ({ origin, pathname }) => {
   const response = await axios.get(pathname, { baseURL: origin });
   const { data } = response;
   return data;
 };
 
-const saveContent = (fullPath, content) => writeFile(fullPath, content);
-
-const createFileName = (pageAddress) => {
-  const extention = 'html';
-  const url = new URL(pageAddress);
-  const { hostname, pathname } = url;
-  const filteredHostname = hostname.split('.');
-  const filteredPathName = pathname.split('/').filter((page) => !!page);
-  const fileName = [
-    ...filteredHostname,
-    ...filteredPathName,
-  ].join('-');
-
-  return `${fileName}.${extention}`;
-};
-
-export default async (url, directoryPath) => {
+export default async (pageAddress, directoryPath) => {
   try {
-    const validUrl = validateUrl(url);
-    const fileNameForSaving = createFileName(validUrl);
-    const fullPath = path.join(directoryPath, fileNameForSaving);
-    const content = await downloadPage(validUrl);
-    await saveContent(fullPath, content);
-    return fullPath;
+    const validUrl = validateUrl(pageAddress);
+    const url = new URL(validUrl);
+    const nameBasePart = url.hostname.split('.').join('-');
+    const htmlFilename = url.pathname
+      .split('/')
+      .filter((pathPart) => !!pathPart)
+      .join('-');
+    const htmlFileExtention = 'html';
+    const savedHtmlFilename = `${nameBasePart}-${htmlFilename}.${htmlFileExtention}`;
+    const pathToSavedHtmlFile = path.join(directoryPath, savedHtmlFilename);
+    const assetsDirectoryName = `${nameBasePart}_files`;
+    const assetsDirectory = path.join(directoryPath, assetsDirectoryName);
+    await mkdir(assetsDirectory);
+
+    const content = await downloadPage(url);
+    const dom = cheerio.load(content);
+    const imageLinks = getImageLinks(dom);
+    const responsesWitImages = await downloadPageImages(url, imageLinks);
+
+    const dataForSavingImages = imageLinks.map((imageLink, index) => {
+      const pathToSavedImageFile = createPathToAssetsFileName(
+        imageLink,
+        nameBasePart,
+        assetsDirectory,
+      );
+      const { data } = responsesWitImages[index];
+      return { name: pathToSavedImageFile, data };
+    });
+
+    dataForSavingImages.forEach(async ({ name, data }) => {
+      await saveContent(name, data);
+    });
+
+    const changedDom = getReplacedDom(dom, nameBasePart, assetsDirectoryName);
+    await saveContent(pathToSavedHtmlFile, changedDom.html());
+    return pathToSavedHtmlFile;
   } catch (e) {
     console.error(e);
     return '';
