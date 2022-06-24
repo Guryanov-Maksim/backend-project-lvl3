6b3136dc13e135e191fb69392cd16cdd64edd4fa
Maksim-Guryanov <guryanov-mv@yandex.ru>
2022-06-24 16:48:25 +0300

    fix: change async/await on then/cathc in bin

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -8,18 +8,19 @@ program
   .version('1.0.0', '-V, --version', 'output the version number')
   .argument('<url>', 'a page address that will be used to download the page html')
   .option('-o, --output [dir]', 'output dir', process.cwd())
-  .action(async (url) => {
-    try {
-      const { output } = program.opts();
-      const savedPagePath = await loadPageAndGetSavedPagePath(url, output);
-      console.log(`Page was successfully downloaded into ${savedPagePath}`);
-      const successAppCode = 0;
-      process.exit(successAppCode);
-    } catch (error) {
-      const failedAppCode = 1;
-      console.error(error.message);
-      process.exit(failedAppCode);
-    }
+  .action((url) => {
+    const { output } = program.opts();
+    loadPageAndGetSavedPagePath(url, output)
+      .then((savedPagePath) => {
+        console.log(`Page was successfully downloaded into ${savedPagePath}`);
+        const successAppCode = 0;
+        process.exit(successAppCode);
+      })
+      .catch((error) => {
+        const failedAppCode = 1;
+        console.error(error.message);
+        process.exit(failedAppCode);
+      });
   });
 
-await program.parseAsync();
+program.parse();
